aef2666e08b6ce7f7bf1bdfdca759d39c8ad60dd
Ashutosh Krishna <ashutoshbritish@gmail.com>
2023-02-25 14:39:55 +0530

    Add fixtures

--- defining-fixtures/test_notes_app.py
+++ defining-fixtures/test_notes_app.py
@@ -1,5 +1,24 @@
+import pytest
+
+from notes_app import NotesApp
+
+
+@pytest.fixture
+def app_without_notes():
+    app = NotesApp()
+    return app
+
+
+@pytest.fixture
+def app_with_notes():
+    app = NotesApp()
+    app.add_note("Test note 1")
+    app.add_note("Test note 2")
+    return app
+
+
 def test_add_note(app_without_notes):
-    result = app_without_notes.add_note("Test note 1")
+    result = app_without_notes.add_note("Test note 1")  
     assert result == "Note added successfully"
     assert len(app_without_notes.notes_list) == 1
     assert app_without_notes.notes_list[0].content == "Test note 1"
